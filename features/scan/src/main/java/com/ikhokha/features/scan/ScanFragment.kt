d726dbe7ffe116d3733e01ea01cecc70713e0a2d
Tshepo Mahlaula <tshepo.mahlaula@multichoice.com>
2022-07-13 11:08:17 +0200

    pre nav fix

--- features/scan/src/main/java/com/ikhokha/features/scan/ScanFragment.kt
+++ features/scan/src/main/java/com/ikhokha/features/scan/ScanFragment.kt
@@ -75,7 +75,7 @@ class ScanFragment : TopNavigationFragment() {
             scanViewModel.checkCartItems()
 
             //Todo: definately remove
-            delay(1000)
+            delay(2000)
             withContext(Dispatchers.Main) {
                 scanner_view?.visibility = View.VISIBLE
             }
